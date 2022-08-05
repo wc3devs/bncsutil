4fbf989ffc9306f708ba0e7a84ec36edea499d61
edelwud <edelwud@icloud.com>
2022-08-05 17:22:33 +0300

    fix(cmake): remove unused comment

--- cmake/mpir.cmake
+++ cmake/mpir.cmake
@@ -12,7 +12,7 @@ else()
     set(OS "macos")
 endif()
 
-add_library(mpir STATIC IMPORTED) # or STATIC instead of SHARED
+add_library(mpir STATIC IMPORTED)
 set_target_properties(mpir PROPERTIES
     IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/lib/libmpir.a"
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/include"
