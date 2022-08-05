31bba8e6a7956232a3a64f907ab3a2ea6d572e77
edelwud <edelwud@icloud.com>
2022-08-05 17:17:55 +0300

    feat(cmake): add cmake mpir finder

---
+++ cmake/mpir.cmake
@@ -0,0 +1,19 @@
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(arch "x64")
+else()
+    set(arch "x32")
+endif()
+
+if (WIN32)
+    set(OS "windows")
+elseif(UNIX)
+    set(OS "linux")
+else()
+    set(OS "macos")
+endif()
+
+add_library(mpir STATIC IMPORTED) # or STATIC instead of SHARED
+set_target_properties(mpir PROPERTIES
+    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/lib/libmpir.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/include"
+)
