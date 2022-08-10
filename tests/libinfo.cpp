535ce32048b0e80b2f349a0616e0524d893d13fa
Maxim Ersh <edelwud@icloud.com>
2022-08-11 02:12:10 +0300

    feat(tests): add library version testing

--- tests/libinfo.cpp
+++ tests/libinfo.cpp
@@ -3,6 +3,10 @@
 
 TEST_SUITE_BEGIN("libinfo");
 
-
+TEST_CASE("currentLibraryVersion") {
+  char version[BUFSIZ];
+  bncsutil_getVersionString(version);
+  MESSAGE("Version: ", version);
+}
 
 TEST_SUITE_END();
