b107a894b5a09f354a253264ac461b2451ab60e2
Maxim Ersh <edelwud@icloud.com>
2022-08-10 22:50:05 +0300

    fix(deps): correct mpir path

--- cmake/mpir.cmake
+++ cmake/mpir.cmake
@@ -6,14 +6,17 @@ endif()
 
 if (WIN32)
     set(OS "windows")
+    set(EXTENSION "lib")
 elseif(UNIX)
     set(OS "linux")
+    set(EXTENSION "a")
+    set(PREFIX "lib")
 else()
     set(OS "macos")
 endif()
 
 add_library(mpir STATIC IMPORTED)
 set_target_properties(mpir PROPERTIES
-    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/lib/libmpir.a"
+    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/lib/${PREFIX}mpir.${EXTENSION}"
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/vcpkg_installed/${arch}-${OS}/include"
 )
