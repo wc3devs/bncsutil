af6bffee912f78c2deebde3dd9e6c77dc964b134
Maxim Ersh <edelwud@icloud.com>
2022-08-11 02:11:38 +0300

    fix(tests): remove usused tests

--- tests/decodekey.cpp
+++
@@ -1,8 +0,0 @@
-#include <doctest/doctest.h>
-#include <bncsutil/bncsutil.h>
-
-TEST_SUITE_BEGIN("decodekey");
-
-
-
-TEST_SUITE_END();
--- tests/oldauth.cpp
+++
@@ -1,8 +0,0 @@
-#include <doctest/doctest.h>
-#include <bncsutil/bncsutil.h>
-
-TEST_SUITE_BEGIN("oldauth");
-
-
-
-TEST_SUITE_END();
